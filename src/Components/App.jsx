5661b3ad24dad28c3809a2ed14536c44f1234b95
JuanMaHern <ju4n.m4rtin.18@gmail.com>
2023-06-19 17:17:06 -0300

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import "../Styles/App.css"
+import BookComponent from "./BookComponent"
+function App() {
+
+  return (
+    <div className="app">
+      <BookComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/BookComponent.jsx
@@ -0,0 +1,8 @@
+import "../Styles/BookComponent.css"
+
+const BookComponent = () => {
+    return(
+        <div className="bookComponent"></div>
+    )
+}
+export default BookComponent
